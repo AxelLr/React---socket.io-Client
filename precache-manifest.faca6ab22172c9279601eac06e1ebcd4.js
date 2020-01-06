eab7042149967e4213163783829933a91b0c3437
Axel <axelleonr12@gmail.com>
2020-01-06 17:20:14 -0300

    Updates

---
+++ precache-manifest.faca6ab22172c9279601eac06e1ebcd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea397dd2200a87565e1cbdad53214f3b",
+    "url": "/React---socket.io-Client/index.html"
+  },
+  {
+    "revision": "dd0c605b342a87e40043",
+    "url": "/React---socket.io-Client/static/css/main.f4c62d28.chunk.css"
+  },
+  {
+    "revision": "e7ceb4359959c8cde788",
+    "url": "/React---socket.io-Client/static/js/2.7176a12d.chunk.js"
+  },
+  {
+    "revision": "b0c52feef59da93649bf0686775e376e",
+    "url": "/React---socket.io-Client/static/js/2.7176a12d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd0c605b342a87e40043",
+    "url": "/React---socket.io-Client/static/js/main.228c336a.chunk.js"
+  },
+  {
+    "revision": "508d98db2cecb43e1bf2",
+    "url": "/React---socket.io-Client/static/js/runtime-main.d20a6bda.js"
+  }
+]);
